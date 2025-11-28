389daf5809bcdf54ba3ab1d4ba4cedab2dc60a0b
dokobhai <samarhardab@gmail.com>
2025-11-27 22:31:34 -0800

    Add more registers for features
    Added registers a, b, c into the adder to prepare for the XOR function in the cpu

--- src/adder.v
+++ src/adder.v
@@ -1,10 +1,21 @@
 module adder ();
 
   reg clk;
+  reg a;
+  reg b;
+  output c;
 
   initial begin
     clk = 0;
     forever #5 clk = ~clk;
+  end
+
+  assign c = a ^ b;
+
+  initial begin
+    a = 1;
+    #10
+    b = 1;
   end
 
   initial begin
