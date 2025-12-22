f0ac6fe38d5209350003d0f411d2c54e45066ae6
DokoBhai <159497103+DokoBhai@users.noreply.github.com>
2025-12-21 21:19:07 -0800

    Modify subtractor testbench for 8-bit inputs

--- tb/subtractor_tb.v
+++ tb/subtractor_tb.v
@@ -1,12 +1,12 @@
 module subtractor_tb ();
 
-reg [3:0] a;
-reg [3:0] b;
+reg [7:0] a;
+reg [7:0] b;
 reg bin;
-wire [3:0] diff;
+wire [7:0] diff;
 wire bout;
 
-subtractor4 dut(
+subtractor8 dut(
   .a(a),
   .b(b),
   .bin(bin),
@@ -18,8 +18,11 @@ initial begin
   $dumpfile("build/subtractor.vcd");
   $dumpvars(0, subtractor_tb);
 
-  a = 4'b0001; b = 4'b0001; bin = 0; #10;
-  a = 4'b0010; b = 4'b0001; bin = 0; #10;
+  a = 8'b00000001; b = 8'b00000001; bin = 0; #10; // 1 - 1
+  a = 8'b00000010; b = 8'b00000001; bin = 0; #10; // 2 - 1
+  a = 8'b00000001; b = 8'b00000010; bin = 0; #10; // borrow
+  a = 8'b00001000; b = 8'b00000001; bin = 1; #10; // with bin
+  a = 8'b00000000; b = 8'b00000001; bin = 0; #10; // underflow
 
   #10 $finish;
 end
