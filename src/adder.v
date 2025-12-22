78cf2101a58c23bfd31ccca0702d24d2f750c976
DokoBhai <159497103+DokoBhai@users.noreply.github.com>
2025-12-21 17:10:02 -0800

    Document 1-bit adder

--- src/adder.v
+++ src/adder.v
@@ -1,3 +1,10 @@
+/*
+This is the module used in adder8.v.
+It only adds 2 single bits and has a carry output and carry input for adder8.v to be a full adder.
+There are 3 inputs, cin, a, b used they all are added together to determine the outputs.
+There are 2 outputs sum, carry these outputs are 1 bit. 
+*/
+
 module adder (
   input wire a, 
   input wire b,
